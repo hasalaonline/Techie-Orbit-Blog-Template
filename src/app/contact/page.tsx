9ffedcfe78a4b717715b3537917292d871e2870a
Hasala Abhilasha <87168716+hasalaonline@users.noreply.github.com>
2024-06-30 14:40:54 +0530

    fix: deployement issues

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,6 +1,6 @@
 "use client";
-import Header from "@/components/organisms/header";
-import Footer from "@/components/organisms/footer";
+import Header from "../../components/organisms/header";
+import Footer from "../../components/organisms/footer";
 
 const Contact = ({ params }: { params: any }) => {
   const { slug } = params;
--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,7 +1,7 @@
-import Header from "@/components/organisms/header";
-import Footer from "@/components/organisms/footer";
-import Hero from "@/components/organisms/hero";
-import Posts from "@/components/organisms/posts";
+import Header from "../../components/organisms/header";
+import Footer from "../../components/organisms/footer";
+import Hero from "../../components/organisms/hero";
+import Posts from "../../components/organisms/posts";
 
 export default function Home() {
   return (
