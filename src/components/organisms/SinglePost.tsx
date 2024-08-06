4a7e77dfafe5026d3d249cdbe0d9fb483adc3e4a
thimiraonline <thimirathenuwara@gmail.com>
2024-08-06 11:48:12 +0530

    💄 Updated kg-cards and other functions in the single post views

--- src/components/organisms/SinglePost.tsx
+++ src/components/organisms/SinglePost.tsx
@@ -11,7 +11,7 @@ const PostPage: React.FC<PostPageProps> = ({ post }) => {
   const sanitizedHtml = DOMPurify.sanitize(post.content ?? "");
 
   return (
-    <div className="post-body w-full flex justify-center mt-4 sm:mt-20 px-4">
+    <div className="post-body gh-content w-full flex justify-center mt-4 sm:mt-20 px-4">
       <div className="w-full max-w-[1000px]">
         <h1 className="text-2xl sm:text-4xl font-bold mb-6 sm:mb-10">
           {post.title}
@@ -30,7 +30,7 @@ const PostPage: React.FC<PostPageProps> = ({ post }) => {
           className="w-full rounded-2xl mb-6 sm:mb-8"
         />
         <div
-          className="prose prose-sm sm:prose lg:prose-lg"
+          className="gh-content prose prose-sm sm:prose lg:prose-lg"
           dangerouslySetInnerHTML={{ __html: sanitizedHtml }}
         />
       </div>
