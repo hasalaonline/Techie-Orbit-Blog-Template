2da0922224455b8278c632f8ed81b850f202887a
Hasala Abhilasha <87168716+hasalaonline@users.noreply.github.com>
2024-08-06 18:32:57 +0530

    feat: contact, about, privacy-policy pages integrated

---
+++ src/app/api/page/route.ts
@@ -0,0 +1,30 @@
+import axios from "axios";
+import { NextRequest } from "next/server";
+
+const api = axios.create({
+  baseURL: "https://demo.ghost.io/ghost/api/content",
+  params: {
+    key: process.env.GHOST_API_KEY,
+  },
+});
+
+export async function GET(request: Request | NextRequest) {
+  const baseURL = process.env.BASE_URL || "http://localhost:3000";
+  const url = new URL(request.url.toString(), baseURL);
+
+  const slug = url.searchParams.get("slug");
+
+  try {
+    const response = await api.get("/pages/slug/" + slug);
+
+    return new Response(JSON.stringify(response.data.pages), {
+      headers: { "Content-Type": "application/json" },
+      status: 200,
+    });
+  } catch (error) {
+    return new Response(JSON.stringify({ error: error }), {
+      headers: { "Content-Type": "application/json" },
+      status: 500,
+    });
+  }
+}
--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,14 +1,55 @@
 "use client";
+import { useQuery } from "@tanstack/react-query";
 import Header from "../../components/organisms/Header";
 import Footer from "../../components/organisms/footer";
+import { TailSpin } from "react-loader-spinner";
+import DOMPurify from "dompurify";
 
-const Contact = ({ params }: { params: any }) => {
+const Contact = () => {
+  const { data, isLoading, error } = useQuery({
+    queryKey: ["contact"],
+    queryFn: async () => {
+      const response = await fetch(`/api/page?slug=contact`);
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+      return response.json();
+    },
+  });
+
+  if (isLoading)
+    return (
+      <div className="flex items-center justify-center h-screen">
+        <TailSpin
+          height="80"
+          width="80"
+          color="#4fa94d"
+          ariaLabel="tail-spin-loading"
+          radius="1"
+          wrapperStyle={{}}
+          wrapperClass=""
+          visible={true}
+        />
+      </div>
+    );
+
+  if (error) return <p>Error: {error.message}</p>;
+
+  if (!data) return <p>No data found</p>;
+
+  const sanitizedHtml = DOMPurify.sanitize(data[0]?.html ?? "");
+
   return (
     <>
       <Header />
-      <h2 className="flex items-center justify-center h-screen">
-        Coming Soon....
-      </h2>
+      <div className="post-body gh-content w-full flex justify-center mt-4 sm:mt-20 px-4">
+        <div className="w-full max-w-[1000px]">
+          <div
+            className="gh-content prose prose-sm sm:prose lg:prose-lg"
+            dangerouslySetInnerHTML={{ __html: sanitizedHtml }}
+          />
+        </div>
+      </div>
       <Footer />
     </>
   );
